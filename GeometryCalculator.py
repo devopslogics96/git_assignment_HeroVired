3b2a00cb5ea9e9f6ead1048f88355940382dfb2f
shubh2096 <shubhgupta925@gmail.com>
2026-03-21 15:36:02 +0530

    WIP: Uncomment length

--- GeometryCalculator.py
+++ GeometryCalculator.py
@@ -20,7 +20,9 @@ if __name__ == "__main__":
 
     print(f"The area of the circle with radius {radius} = {calculator.calculate_circle_area(radius)}")
 
-# TODO: Implement the feature to calculate the area of a rectangle # length = 10
+# TODO: Implement the feature to calculate the area of a rectangle # 
+    
+    length = 10
 
     width = 6
 
